28418e84b7e7102804188259154f3587ecb46855
Antonio Solo <as@solotony.com>
2020-09-26 12:55:50 +0300

    shedule

--- transkit/__main__.py
+++ transkit/__main__.py
@@ -3,7 +3,10 @@ import sys
 from transkit.bot import Bot
 import requests
 import logging
-from datetime import datetime
+import pytz
+from datetime import datetime, timezone, tzinfo
+import json
+
 from .globals import PROD, DRIVER, PROFILE, DELAY_MIN, DELAY_MAX
 
 LOGIN = 'arttronic' if PROD else 'login'
@@ -16,35 +19,63 @@ COMPARE_TRESHOLD = 0.019 # от 0 до 1
 RANDOM_MOVE_TRESHOLD = 0.073 # от 0 до 1
 HEADEROFFSET=-170
 
-def main(args):
+local_day = datetime.now().astimezone(tz=pytz.timezone('Europe/Moscow')).strftime('%Y%m%d')
 
-    logging.basicConfig( filename="log/transkit-parse-{}.log".format(datetime.now().strftime('%Y%m%d%H%M')), level=logging.INFO )
+def failed(text):
+    logging.error(text)
+    exit(0)
+    pass
+
+def main(args):
+    # set a format which is simpler for console use
+    formatter = logging.Formatter("%(asctime)s;%(levelname)s;%(message)s")
+    logging.basicConfig( filename="log/transkit-parse-{}.log".format(datetime.now().strftime('%Y%m%d%H%M')), level=logging.INFO, format="%(asctime)s;%(levelname)s;%(message)s")
+    console = logging.StreamHandler()
+    console.setLevel(logging.INFO)
+    # tell the handler to use this format
+    console.setFormatter(formatter)
+    # add the handler to the root logger
+    logging.getLogger().addHandler(console)
 
     if len(args) < 2:
-        print('python -m transkit <имя_файла>')
-        exit(1)
+        failed('формат запуска: python -m transkit shedule|<имя_файла>')
 
     transmissions = []
-    with open(args[1], 'r') as transmissions_file:
-        transmissions = [row.strip() for row in transmissions_file]
+
+    if args[1]=='shedule':
+        logging.info('Загружаем расписание')
+        try:
+            ulr = 'https://mskakpp.ru/parsers/transkit-shedule/{}/'.format(local_day)
+            r = requests.get(ulr)
+            if r.status_code!=200:
+                failed('Ошибка получения данных с сервера: {}'.format(r.status_code))
+            print(json.loads(r.text))
+        except Exception as e:
+            failed('Ошибка при получения данных с сервера: {}'.format(str(e)))
+    else:
+        with open(args[1], 'r') as transmissions_file:
+            transmissions = [row.strip() for row in transmissions_file]
+
+    logging.info('Начинаем сканирование')
+    ulr = 'https://mskakpp.ru/parsers/transkit-start/{}/'.format(local_day)
+    requests.post(ulr)
+
+    exit(0)
 
     bot = Bot('http://www.transkit.ru/', PROD, DRIVER, PROFILE)
 
     if bot.find(id='menuIconAccountActiveArea'):
-        print('  INFO уже залогинены')
         logging.info('  уже залогинены')
     else:
-        print('  INFO надо логиниться')
         logging.info('  надо логиниться')
         process_login(bot)
 
         if PROD:
             if bot.find(id='menuIconAccountActiveArea'):
-                print('  INFO залогинены успешно')
                 logging.info('  надо логиниться')
             else:
                 logging.critical('логин ахтунг!!!!!!!!!!!!')
-                exit('  CRITICAL логин ахтунг!!!!!!!!!!!!')
+                exit('логин ахтунг!!!!!!!!!!!!')
 
     first = True
     for transmission in transmissions:
@@ -52,21 +83,19 @@ def main(args):
             bot.sleep(random.randint(DELAY_MIN, DELAY_MAX))
         first = False
         logging.info('начинаем коробку {}'.format(transmission))
-        print('начинаем коробку {}'.format(transmission))
         process_transmission(bot, transmission)
         logging.info('завершена коробка {}'.format(transmission))
-        print('завершена коробка {}'.format(transmission))
 
-
+    logging.info('завершено сканирование')
+    ulr = 'https://mskakpp.ru/parsers/transkit-stop/{}/'.format(local_day)
+    requests.post(ulr)
 
 def do_compare(bot):
     logging.info('Выполняется сравнение')
-    print('Выполняется сравнение')
 
     compare_button = bot.find(xpath='//td[@id="topMenuCMPButton"]/a')
     if not compare_button:
         logging.error('  Не удается найти кнопку перехода в сравнение')
-        print('  ERROR Не удается найти кнопку перехода в сравнение')
         return False
 
     bot.move_to(compare_button, 'compare_button', scroll=False, randomize=5, ybaseoffset=HEADEROFFSET)
@@ -75,12 +104,10 @@ def do_compare(bot):
     clean_link = bot.find(link_text_part='Очистить список сравнения')
 
     if not clean_link:
-        print('  ERROR не удается найти кнопку очистки сравнения')
         logging.error('  не удается найти кнопку очистки сравнения')
         bot.back()
         return False
 
-    print('Очистка сравнения')
     logging.info('Очистка сравнения')
     bot.move_to(clean_link, 'clean_link', scroll=False, randomize=5)
     bot.click_at(clean_link, 'clean_link')
@@ -90,12 +117,10 @@ def do_compare(bot):
     return True
 
 def do_view(bot, part_name):
-    print('Выполняется просмотр запчасти')
     logging.info('Выполняется просмотр запчасти')
     a_link = bot.find(link_text=part_name)
     if not a_link:
         logging.error('ссылка на страницу запчасти не найдена')
-        print('  ERROR ссылка на страницу запчасти не найдена')
         return False
 
     bot.move_to(a_link, 'a_link', scroll=False, randomize=5)
@@ -108,30 +133,26 @@ def do_view(bot, part_name):
     return True
 
 def process_login(bot):
-    print('PROD={}'.format(PROD))
+    logging.debug('PROD={}'.format(PROD))
 
     button_link = bot.find(id='hiddenAuthBut')
     if not button_link:
-        logging.critical('Не могу найти кнопку для открытия формы входа')
-        exit('  CRITICAL Не могу найти кнопку для открытия формы входа')
+        failed('Не могу найти кнопку для открытия формы входа')
 
     bot.move_to(button_link, 'button_link', scroll=False, randomize=5)
     bot.click_at(button_link, 'button_link')
 
     login_input = bot.find(id='login')
     if not button_link:
-        logging.critical('Не могу найти поле для логина')
-        exit('  CRITICAL Не могу найти поле для логина')
+        failed('Не могу найти поле для логина')
 
     password_input = bot.find(name='password')
     if not button_link:
-        logging.critical('Не могу найти поле для пароля')
-        exit('  CRITICAL Не могу найти поле для пароля')
+        failed('Не могу найти поле для пароля')
 
     button_enter = bot.find(xpath='//button[text()="Войти"]')
     if not button_link:
-        logging.critical('Не могу найти кнопку "войти')
-        exit('  CRITICAL Не могу найти кнопку "войти"')
+        failed('Не могу найти кнопку "войти"')
 
     if PROD:
         logging.info('do login')
@@ -155,18 +176,16 @@ def process_transmission(bot, transmission):
     transmission = transmission.strip()
     if not transmission:
         logging.error('Коробка не указана')
-        print('  Коробка не указана')
         return False
 
     bot.sleep(random.randint(7, 15))
 
-    print('Коробка {}'.format(transmission))
+    logging.info('начинаем коробку {}'.format(transmission))
     num_compare = 0
 
     search_input = bot.find(id='dsch')
     if not search_input:
-        logging.critical('Не могу найти поле поиска')
-        exit('  CRITICAL Не могу найти поле поиска')
+        failed('Не могу найти поле поиска')
 
     search_button = None
 
@@ -177,7 +196,7 @@ def process_transmission(bot, transmission):
 
     if not search_button:
         logging.critical('Не могу кнопку поиска')
-        exit('  CRITICAL Не могу кнопку поиска')
+        failed('Не могу кнопку поиска')
 
     bot.move_to(search_input, 'search_input', scroll=False, randomize=5)
     search_input.clear()
@@ -204,7 +223,6 @@ def process_transmission(bot, transmission):
                 view_mode_button = bot.find(xpath='//*[@title="Показать все детали трансмиссии в виде таблицы"]')
                 if not view_mode_button:
                     logging.error('кнопка режима просмотра не найдена')
-                    print('  ERROR кнопка режима просмотра не найдена')
                     return False
 
     bot.click_at(view_mode_button, 'view_mode_button')
@@ -213,7 +231,6 @@ def process_transmission(bot, transmission):
     tag_table = soup.find('table', attrs={'id': 'detailstable'})
     if not tag_table:
         logging.error('таблица деталей не найдена')
-        print('  ERROR таблица деталей не найдена')
         return False
 
     parts = []
@@ -221,12 +238,10 @@ def process_transmission(bot, transmission):
         tag_tr_class = tag_tr.get('class')
         if tag_tr_class and 'transtabletopbg' in tag_tr_class:
             logging.info('  пропускаем заголовок таблицы')
-            print('  INFO пропускаем заголовок таблицы')
             continue
 
         # if tag_tr.find('button', attrs={'class': 'emailMe'}, recursive=True):
         #     logging.info('  пропускаем деталь нет в наличии')
-        #     print('  INFO пропускаем деталь нет в наличии')
 
         articul, cmp_div_id, cmp_calc_price, price_at_page = None, None, None, None
         for counter, tag_td in enumerate(tag_tr.findChildren('td', recursive=False)):
@@ -251,7 +266,6 @@ def process_transmission(bot, transmission):
         if articul:
             parts.append((articul, cmp_div_id, cmp_calc_price, price_at_page))
             logging.info('  найден компонент {} ({}) '.format(articul, price_at_page))
-            print('  INFO найден компонент {} ({}) '.format(articul, price_at_page))
 
     for counter, part in enumerate(parts):
         if part[3]:
@@ -263,7 +277,6 @@ def process_transmission(bot, transmission):
                     'token': 'x777xx777x'
                 }
                 r = requests.post('https://mskakpp.ru/catalog/api/update-transkit/', json=data)
-                print('  SITE UPDATE:', r.status_code, r.content)
                 logging.info('update: {}'.format(str(data)))
 
     for counter,part in enumerate(parts):
@@ -272,7 +285,6 @@ def process_transmission(bot, transmission):
             bot.move_to_random()
 
         logging.info('Обрабатываем деталь `{}` `{}` `{}`'.format(part[0],part[1],part[2]))
-        print('Обрабатываем деталь `{}` `{}` `{}`'.format(part[0],part[1],part[2]))
 
         if random.random() <= VIEW_TRESHOLD:
             do_view(bot, part[0])
@@ -281,13 +293,11 @@ def process_transmission(bot, transmission):
             cmp_input = bot.find(xpath='//*[@id="{}"]/form/input[3]'.format(part[1]))
             if cmp_input:
                 logging.info('  найдена кнопка "в сравнение"')
-                print('  INFO найдена кнопка "в сравнение"')
                 bot.move_to(cmp_input, 'cmp_input', scroll=False, randomize=5, ybaseoffset=HEADEROFFSET)
                 bot.click_at(cmp_input, 'cmp_input')
                 num_compare += 1
             else:
                 logging.error('  не найдена кнопка "в сравнение"')
-                print('  ERROR не найдена кнопка "в сравнение"')
 
             if num_compare >= random.randint(NUM_COMPARE_MIN, NUM_COMPARE_MAX):
                 if do_compare(bot):
@@ -300,18 +310,16 @@ def process_transmission(bot, transmission):
             price_span = bot.find(id=part[2])
             if not price_span:
                 logging.error('  не найден элемент рассчитать {}'.format(part[2]))
-                print('  ERROR не найден элемент рассчитать {}'.format(part[2]))
             else:
                 bot.move_to(price_span, 'price_span', scroll=False, randomize=5, ybaseoffset=HEADEROFFSET)
                 bot.click_at(price_span, 'price_span')
                 price_span = bot.find(xpath='//span[@id="{}"]/span'.format(part[2]))
                 if not price_span:
                     logging.error('  не найден элемент с ценой для {}'.format(part[2]))
-                    print('  ERROR не найден элемент с ценой для {}'.format(part[2]))
                 else:
                     if PROD:
                         price = price_span.get_attribute('innerHTML')
-                        print('  YAHOO {}:{}'.format(part[0], price))
+                        logging.debug('  YAHOO {}:{}'.format(part[0], price))
                         data = {
                             'transmission': transmission,
                             'partno': part[0],
@@ -319,7 +327,6 @@ def process_transmission(bot, transmission):
                             'token':'x777xx777x'
                         }
                         r = requests.post('https://mskakpp.ru/catalog/api/update-transkit/', json=data)
-                        print('  SITE UPDATE:', r.status_code, r.content)
                         logging.info('update: {}'.format(str(data)))
         else:
             if PROD:
@@ -330,7 +337,6 @@ def process_transmission(bot, transmission):
                     'token': 'x777xx777x'
                 }
                 r = requests.post('https://mskakpp.ru/catalog/api/update-transkit/', json=data)
-                print('  SITE UPDATE:', r.status_code, r.content)
                 logging.info('update: {}'.format(str(data)))
 
     # полюбому перед выходом!!
