0b7dc617e5b2794c7ac875906703319fb88620c7
Antonio Solo <as@solotony.com>
2020-09-29 10:31:35 +0300

    virtual screen

--- test.py
+++ test.py
@@ -14,10 +14,14 @@ class MyProfile(webdriver.firefox.firefox_profile.FirefoxProfile):
 #driver = webdriver.Firefox(firefox_profile=MyProfile())
 #driver = webdriver.Firefox(FirefoxProfile("/home/pi/.mozilla/firefox/ofvss46d.default-esr"))
 display = Display(visible=0, size=(1280, 1024))
+print('1 OK')
 display.start()
-
+print('2 OK')
 driver = webdriver.Firefox()
+print('3 OK')
 driver.maximize_window()
+print('4 OK')
 driver.get("https://solotony.com/tools/ip/")
-
+print('5 OK!!!')
 display.stop()
+print('6 OK')
